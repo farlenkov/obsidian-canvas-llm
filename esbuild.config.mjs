5e9ffe96a633d605baa0854ced1310e923b70f98
mike <mike@farlenkov.com>
2025-08-03 00:08:20 +0300

    tune building

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -11,7 +11,7 @@ if you want to view the source, please visit the github repository of this plugi
 `;
 
 const prod = (process.argv[2] === "production");
-const outdir = "../../vault/.obsidian/plugins/obsidian-canvas-llm";
+const outdir = "../vault_dev/.obsidian/plugins/obsidian-canvas-llm";
 
 async function copyStatic() 
 {
